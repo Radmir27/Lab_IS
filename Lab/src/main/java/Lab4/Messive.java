a9115e567275b91a7f972b78123f23d5c3212db7
Abkarimov <abkarimov2001@outlook.com>
2021-02-27 16:50:18 +0400

    Load the Lab folder

---
+++ Lab/src/main/java/Lab4/Messive.java
@@ -0,0 +1,12 @@
+package Lab4;
+
+public class Messive {
+	
+	public void prisvoenie() {
+		int[] a = new int[9];
+		for (int i = 0; i < 9; i++) {
+			a[i] = (int) (Math.random()*70 + 20);
+		}
+	}
+
+}
